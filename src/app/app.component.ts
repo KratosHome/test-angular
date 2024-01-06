dfbeb8149ea64a6538fe3676ed2db52fce4df06e
Oleg Tkach <KratosHome101@gmail.com>
2024-01-06 14:06:07 +0200

    routers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {OrdersComponent} from "./pages/orders/orders.component";
+import {ProductsComponent} from "./pages/products/products.component";
+import {MainComponent} from "./pages/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    OrdersComponent,
+    ProductsComponent,
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test';
+  title = 'main';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MainComponent} from "./pages/main/main.component";
+import {OrdersComponent} from "./pages/orders/orders.component";
+import {ProductsComponent} from "./pages/products/products.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'orders',
+    component: OrdersComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+];
